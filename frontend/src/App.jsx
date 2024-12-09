51d97ab59840ccceeb1aa781d965116c1b6395ac
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-12-09 17:29:20 +0000

    added login button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,9 @@ function App() {
         </nav>
         <div className="user-panel">
           <a href="#" style={{ color: "black" }}>
+            Log In
+          </a>
+          <a href="#" style={{ color: "black" }}>
             Search
           </a>
           <a href="#" style={{ color: "black" }}>
