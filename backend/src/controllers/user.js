e5115a2f9acad066c52e4f57c972366c2e53b28d
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-12-10 18:49:15 +0000

    added necessary imports

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -1,8 +1,11 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { PrismaClientKnownRequestError } = require("@prisma/client");
 const { registerUserDb} = require("../domains/user")
 
 const registerUser = async (req, res) => {
-    console.log("Hi There from controller")
-try {
+    const {username, password} = req.body;
+    try {
 
 } catch (error) {
     console.log("Error", error);
