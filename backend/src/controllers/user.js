c5829d58ad7ce16d316b369a366ac4b85a91310a
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-12-11 18:56:27 +0000

    updated format for controller user.js

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -1,18 +1,16 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { PrismaClientKnownRequestError } = require("@prisma/client");
-const { registerUserDb} = require("../domains/user")
+const { registerUserDb } = require("../domains/user");
 
 const registerUser = async (req, res) => {
-    const {username, password} = req.body;
-    try {
-
-} catch (error) {
+  const { username, password } = req.body;
+  try {
+  } catch (error) {
     console.log("Error", error);
-}
-
+  }
 };
 
 module.exports = {
-    registerUser
-}
+  registerUser,
+};
