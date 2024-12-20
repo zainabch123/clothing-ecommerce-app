56464ef5bf36538e9fd84fd98cc56bbf3ec384da
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-12-20 15:52:16 +0000

    updated domains file formatting

--- backend/src/domains/user.js
+++ backend/src/domains/user.js
@@ -1,25 +1,23 @@
 const prisma = require("../utils/database");
 
-const registerUserDb = async(username, hashedPassword) => {
-    return await prisma.user.create({
-        data: {
-            username: username,
-            password: hashedPassword,
-        }
-    });
-
+const registerUserDb = async (username, hashedPassword) => {
+  return await prisma.user.create({
+    data: {
+      username: username,
+      password: hashedPassword,
+    },
+  });
 };
 
-const loginUserDb = async(username) => {
-    return await prisma.user.findUnique({
-        where: {
-            username: username
-        }
-    });
-}
-
+const loginUserDb = async (username) => {
+  return await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+};
 
 module.exports = {
-    registerUserDb,
-    loginUserDb,
-}
+  registerUserDb,
+  loginUserDb,
+};
